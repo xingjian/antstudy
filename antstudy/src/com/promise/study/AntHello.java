60fefe59c9dd462bdeb183e58bfe90d56c7d432e
xingjian <xingjian@9a834573-2090-804c-9287-1a834bdb27e7>
2014-10-26 12:17:25 +0000

    git-svn-id: svn://10.10.191.1:3690/AntStudy/trunk@775 9a834573-2090-804c-9287-1a834bdb27e7

---
+++ antstudy/src/com/promise/study/AntHello.java
@@ -0,0 +1,19 @@
+/** 文件名: AntHello.java 创建人：邢健  创建日期： 2014-3-2 上午10:11:21 */
+
+package com.promise.study;
+
+/**   
+ * 类名: AntHello.java 
+ * 包名: com.promise.study 
+ * 描述: Hello Ant 
+ * 作者: xingjian xingjian@yeah.net   
+ * 日期:2014-3-2 上午10:11:21 
+ * 版本: V1.0   
+ */
+public class AntHello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Ant!");
+	}
+
+}
---
+++ antstudy/src/com/promise/study2/Main.java
@@ -0,0 +1,25 @@
+/** @文件名: Main.java @创建人：邢健  @创建日期： 2014-3-4 上午8:18:58 */
+
+package com.promise.study2;
+
+/**   
+ * @类名: Main.java 
+ * @包名: com.promise.study2 
+ * @作者: xingjian xingjian@yeah.net   
+ * @日期:2014-3-4 上午8:18:58 
+ * @版本: V1.0   
+ */
+public class Main {
+
+	/**
+	 * javac编译 java运行
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for(int i=0;i<args.length;i++){
+			System.out.println(args[i]);
+		}
+
+	}
+
+}
